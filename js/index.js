d853bfd9d744fc04bf4a27bd317eaabc1b2cde8f
Sunny <sunny.lan.coder@gmail.com>
2017-08-09 23:32:38 -0400

    Working scroll w/ navbar link animations

--- js/index.js
+++ js/index.js
@@ -2,27 +2,75 @@ Number.prototype.map = function (in_min, in_max, out_min, out_max) {
     return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 };
 
+function performLayout(articles, orderFunc) {
+    //clear old elements
+    $('#info-blocks > *').remove(':not(#about-me)');
+    $('#nav-bar > *').remove(':not(#name-block)');
+
+    var infoBlock = $('#info-blocks');
+    var navBar = $("#nav-bar");
+
+    for (var articleID in articles) if (articles.hasOwnProperty(articleID)) {
+        var article = articles[articleID];
+        article.previewElem = $(
+            "<div " +
+            "id='article-preview-" + articleID + "' " +
+            "class='info-block'><div class='content'>" +
+            article.preview +
+            "</div></div>");
+        infoBlock.append(article.previewElem);
+
+        article.navBarElem = $(
+            "<div id='nav-item-" + articleID + "' class='nav-item'>" +
+            article.navLabel +
+            "</div>");
+        navBar.append(article.navBarElem);
+    }
+}
+
 $(function () {
-    $('.blocks').click(function (e) {
-        console.log('click');
-        $(e.target).remove();
-    });
+    //Load content:
+    var schema = JSON.parse(document.getElementById('injected-schema').innerHTML);
+    var articles = schema.articles;
+    performLayout(articles);
+
+    $('#name-block').addClass('selected');
 
     $(window).scroll(function () {
+        //calculate scroll location
         var s = $(window).scrollTop(),
-            h = $(document).height() - $(window).height();
+            pgHeight = $(window).height(),
+            h = $(document).height() - pgHeight;
 
-        $('.sidebar.background .background').css('top', s.map(0, h, 0, -10) + '%');
-        if (s > 7) {
-            $('.nav-bar-wrapper .nav-bar').css('top', s.map(0, h, 10, -10) + '%');
+        //background image parallax
+        $('.background').css('top', s.map(0, h, 0, -10) + '%');
+
+        // Intro -> Content transitions, navbar parallax
+        if (s > 10) {
+            $('#nav-bar').css('top', s.map(0, h, 35, 10) + '%');
             $('.sidebar').addClass('closed');
-            $('.nav-bar-wrapper').addClass('closed');
-            $('.nav-item').removeClass('selected');
+            $('#nav-bar-wrapper').addClass('closed');
         } else {
-            $('.nav-bar-wrapper .nav-bar').css('top', s.map(0, h, 40, 35) + '%');
+            $('#nav-bar').css('top', s.map(0, h, 40, 35) + '%');
             $('.sidebar').removeClass('closed');
-            $('.nav-bar-wrapper').removeClass('closed');
-            $('.nav-item').addClass('selected');
+            $('#nav-bar-wrapper').removeClass('closed');
+        }
+
+        var viewStart = s,
+            viewEnd = s + pgHeight - 10;
+        console.log("View start:", viewStart, ", View end:", viewEnd);
+
+        // Selected
+        for (var articleID in articles) if (articles.hasOwnProperty(articleID)) {
+            var article = articles[articleID];
+            var element = article.previewElem;
+            var top = element.offset().top,
+                bottom = top + element.height();
+            console.log('Top', top);
+            if (top >= viewStart && top <= viewEnd)
+                article.navBarElem.addClass('selected');
+            else
+                article.navBarElem.removeClass('selected');
         }
     });
 
