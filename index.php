9ae009b9b0122d173f44ca888fdaba56b449a198
Sunny <sunny.lan.coder@gmail.com>
2017-08-15 00:29:39 -0400

    Working down arrow

--- index.php
+++ index.php
@@ -6,11 +6,21 @@
     <meta charset="UTF-8">
     <title>Sunny Lan</title>
     <link rel="stylesheet" type="text/css" href="/css/index.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
+
     <script
-            src="http://code.jquery.com/jquery-3.2.1.min.js"
+            src="https://code.jquery.com/jquery-3.2.1.min.js"
             integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
             crossorigin="anonymous"></script>
+
+    <script
+            src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+            integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+            crossorigin="anonymous"></script>
+
     <script src="/js/index.js"></script>
+
 
     <?php
     echo "<script type = 'application/json' id = 'injected-schema'>";
@@ -27,11 +37,16 @@
 </div>
 
 <div id="bg" class="sidebar">
-    <div class="background"></div>
+    <div class="background parallax"></div>
 </div>
 <div id="nav-bar-wrapper" class="sidebar">
     <div id="nav-bar">
     </div>
+</div>
+
+<div class="down-arrow">
+    <!--<span class="text">Scroll down</span>-->
+    <div class="material-icons arrow">expand_more</div>
 </div>
 
 </body>
--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ Number.prototype.map = function (in_min, in_max, out_min, out_max) {
     return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 };
 
-function performLayout(articles, orderFunc) {
+
+var articles;
+
+function performLayout(orderFunc) {
     var infoBlock = $('#info-blocks');
 
     //clear old elements
@@ -13,6 +16,8 @@ function performLayout(articles, orderFunc) {
 
     var topMostArticleID = null;
     var minOrder = Number.MAX_VALUE;
+
+    var count = 0;
 
     for (var articleID in articles) if (articles.hasOwnProperty(articleID)) {
         var article = articles[articleID];
@@ -39,66 +44,103 @@ function performLayout(articles, orderFunc) {
             article.navLabel +
             "</div>");
         navBar.append(article.navBarElem);
+
+        count++;
     }
 
     //inject top to topmost article
     $("#article-preview-" + topMostArticleID).addClass('top');
     $("#nav-item-" + topMostArticleID).addClass('top');
+
+    return count;
+}
+
+var hideLock;
+
+function scrollCalc() {
+    //calculate scroll location
+    var s = $(window).scrollTop(),
+        pgHeight = $(window).height(),
+        h = $(document).height() - pgHeight;
+
+    //background image parallax
+    $('.background.parallax').css('transform', 'translate(0, ' + s.map(0, h, 0, -10) + '%)');
+
+    // Intro -> Content transitions, nav bar parallax, down arrow fade
+    if (s > 10) {
+        $('#nav-bar').css('top', s.map(0, h, 35, 0) + '%');
+        $('.sidebar').addClass('closed');
+        $('#nav-bar-wrapper').addClass('closed');
+
+        $('.down-arrow').addClass('hide');
+    } else {
+        $('#nav-bar').css('top', s.map(0, h, 40, 35) + '%');
+        $('.sidebar').removeClass('closed');
+        $('#nav-bar-wrapper').removeClass('closed');
+        if (!hideLock)
+            $('.down-arrow').removeClass('hide');
+    }
+
+    // Selected animations
+    var errAllow = 50;
+    var viewStart = s - errAllow,
+        viewEnd = s + pgHeight + errAllow;
+
+    // console.log("View start:", viewStart, "View end:", viewEnd);
+
+    for (var articleID in articles) if (articles.hasOwnProperty(articleID)) {
+        var article = articles[articleID];
+        var element = article.previewElem;
+        var top = element.offset().top,
+            bottom = top + element.height();
+
+        // console.log("top: ", top, "bottom: ", bottom);
+
+        if (top >= viewStart && bottom <= viewEnd)
+            article.navBarElem.addClass('selected');
+        else
+            article.navBarElem.removeClass('selected');
+    }
+}
+
+function resizeCalc() {
+    if ($(document).height() > $(window).height()) {
+        $('.down-arrow').removeClass('hide');
+        hideLock = false;
+        console.log("lol");
+    }
+    else {
+        $('.down-arrow').addClass('hide');
+        hideLock = true;
+    }
+    scrollCalc();
 }
 
 $(function () {
     //Load content:
     var schema = JSON.parse(document.getElementById('injected-schema').innerHTML);
-    var articles = schema.articles;
-    performLayout(articles, function (article) {
+    articles = schema.articles;
+    var count = performLayout(function (article) {
         return article.defaultDisplayOrder;
     });
 
-    function scrollCalc() {
-        //calculate scroll location
-        var s = $(window).scrollTop(),
-            pgHeight = $(window).height(),
-            h = $(document).height() - pgHeight;
 
-        //background image parallax
-        $('.background').css('top', s.map(0, h, 0, -10) + '%');
+    var downArrow = $('.down-arrow');
 
-        // Intro -> Content transitions, nav bar parallax
-        if (s > 10) {
-            $('#nav-bar').css('top', s.map(0, h, 35, 0) + '%');
-            $('.sidebar').addClass('closed');
-            $('#nav-bar-wrapper').addClass('closed');
-        } else {
-            $('#nav-bar').css('top', s.map(0, h, 40, 35) + '%');
-            $('.sidebar').removeClass('closed');
-            $('#nav-bar-wrapper').removeClass('closed');
-        }
+    //auto scroll down
+    downArrow.click(function () {
+        var elem = $('.info-block.top');
+        $('html, body').animate({
+            scrollTop: elem.offset().top + elem.height()
+        }, 1000, "easeOutQuad");
+    });
 
-        // Selected animations
-        var errAllow = 50;
-        var viewStart = s - errAllow,
-            viewEnd = s + pgHeight + errAllow;
-
-        // console.log("View start:", viewStart, "View end:", viewEnd);
-
-        for (var articleID in articles) if (articles.hasOwnProperty(articleID)) {
-            var article = articles[articleID];
-            var element = article.previewElem;
-            var top = element.offset().top,
-                bottom = top + element.height();
-
-            // console.log("top: ", top, "bottom: ", bottom);
-
-            if (top >= viewStart && bottom <= viewEnd)
-                article.navBarElem.addClass('selected');
-            else
-                article.navBarElem.removeClass('selected');
-        }
-    }
+    if (count <= 1)
+        downArrow.addClass('hide');
 
     $(window).scroll(scrollCalc);
+    $(window).resize(resizeCalc);
 
-    //ensure proper scroll calculations on init and resize
-    scrollCalc();
-    $(window).resize(scrollCalc);
+    //ensure proper init calcs
+    resizeCalc();
 });
